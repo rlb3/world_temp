b487c2aafc90208a7d27efdb88f8a490f348c786
Robert Boone <robert@rlb3.com>
2020-08-16 12:42:38 -0500

    Broadway test

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule WorldTemp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :world_temp,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {WorldTemp.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 1.7"},
+      {:jason, "~> 1.2.1"},
+      {:broadway, "0.6.1"}
+    ]
+  end
+end
